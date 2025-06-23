abf10df2d3fc0d77831baf35b62bf07175939012
Yeopsony <ikik6666@naver.com>
2025-06-23 16:47:36 +0900

    backend upload2

--- src/main/java/com/example/birthstone/dto/UserInfo.java
+++ src/main/java/com/example/birthstone/dto/UserInfo.java
@@ -1,4 +1,13 @@
 package com.example.birthstone.dto;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
 public class UserInfo {
+
+    private LocalDate userBirthday;
+    private String name;
+
 }
--- src/main/java/com/example/birthstone/entity/Stone.java
+++ src/main/java/com/example/birthstone/entity/Stone.java
@@ -1,4 +1,28 @@
 package com.example.birthstone.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+@Getter
+@Setter
+@Table(name = "Stone_info")
 public class Stone {
+
+    @Id
+    @Column(name = "month", nullable = false)
+    private Integer month;  // 탄생석월 (PK)
+
+    @Column(name = "name", length = 40)
+    private String name;  // 탄생석명
+
+    @Column(name = "explan", length = 100)
+    private String explan;  // 탄생석 설명
 }
